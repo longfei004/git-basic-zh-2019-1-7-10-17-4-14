bff6fad030c1bc0def38e592386b795fb59911d2
longfei.liu <longfei.liu@thoughtworks.com>
2019-09-05 00:32:35 +0800

    [Liu Longfei] feat: finish the section1 exercise

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,12 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        return collection1.stream().filter(c1 -> collection2.stream().flatMap(Collection::stream).anyMatch(c2 -> c2.equals(c1))).collect(Collectors.toList());
     }
 }
